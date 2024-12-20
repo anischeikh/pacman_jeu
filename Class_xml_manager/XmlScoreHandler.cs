442f80ba664278b3e4c7752ea5d32e98caf39a80
PaulosJaice <68250974+PaulosJaice@users.noreply.github.com>
2024-12-20 14:46:25 +0100

    Update XmlScoreHandler.cs

--- Class_xml_manager/XmlScoreHandler.cs
+++ Class_xml_manager/XmlScoreHandler.cs
@@ -10,7 +10,60 @@ public static class XmlScoreHandler
 
     private static XmlNode _element;
     
+    
+    //hotfix 1
+    public static void InputPlayerInfo(string nomFichierXml, string nomElement, string info)
+    {
+        try
+        {
+            _xmlDoc = new XmlDocument();
+            _xmlDoc.Load(nomFichierXml);
+            _element = _xmlDoc.SelectSingleNode(nomElement);
+            
+            if (_element != null)
+            {
+                _element.InnerText = info.ToString();
+                _xmlDoc.Save(nomFichierXml);
+            }
+            else
+            {
+                Console.WriteLine("Element "+nomElement+" pas trouvé!");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    
+    //hotfix 2
+    public static void InitializeCurrentPelletScore(string nomFichierXml, string nomElement)
+    {
+        try
+        {
+            _xmlDoc = new XmlDocument();
+            _xmlDoc.Load(nomFichierXml);
+            _element = _xmlDoc.SelectSingleNode(nomElement);
+            
+            if (_element != null && int.TryParse(_element.InnerText, out int score))
+            {
+                score=0;
+                _element.InnerText = score.ToString();
+                _xmlDoc.Save(nomFichierXml);
+            }
+            else
+            {
+                Console.WriteLine("Element "+nomElement+" pas trouvé!");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    
     //nom du fichier XML (avec le PATH) et le nom de l'élément (ne pas oublier les //)!!!
+    //incrémente de 1 l'élément voulu
     public static void IncrementElement(string nomFichierXml, string nomElement)
     {
         try
