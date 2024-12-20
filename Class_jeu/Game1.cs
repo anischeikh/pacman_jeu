ade894f66cd391113744e581b5f22092d97287b2
PaulosJaice <68250974+PaulosJaice@users.noreply.github.com>
2024-12-20 15:34:59 +0100

    Update Game1.cs

--- Class_jeu/Game1.cs
+++ Class_jeu/Game1.cs
@@ -74,7 +74,7 @@ namespace pacman.Class_jeu
             points = new List<Point>();
             ghosts = new List<Creature>();
             currentGameState = etatJeu.Menu;
-            XmlScoreHandler.InitializeCurrentPelletScore("Content/src/data/xml/Init_Game.xml","//tot_current_pellets");
+            XmlScoreHandler.InitializeCurrentPelletScore("Content/src/data/xml/Init_Game.xml","//current_pellets_eaten");
             base.Initialize();
         }
 
